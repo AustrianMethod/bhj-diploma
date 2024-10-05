1813160186dd78d66683efaa90dc0a721c93ec69
AustrianMethod <ivanov.nv.spb@gmail.com>
2024-10-05 20:32:40 +0300

    createRequestJS and UserJS modified

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -2,6 +2,46 @@
  * Основная функция для совершения запросов
  * на сервер.
  * */
-const createRequest = (options = {}) => {
-
-};
+/**
+ * Основная функция для совершения запросов
+ * на сервер.
+ * */
+const createRequest = ( options ) => {
+    const {
+      url,
+      data: {
+          email,
+          password,
+          name,
+        },
+    method,
+    callback, //= ( err, response ) => {
+    //       console.log( 'Ошибка:', err );
+    //       console.log( 'Данные:', response );
+    //   }, 
+    } = options;
+  
+    const encodedEmail = encodeURIComponent( email );
+    const encodedPassword = encodeURIComponent( password );
+    const newURL = `${url}?mail=${encodedEmail}&password=${encodedPassword}`;
+  
+    const xhr = new XMLHttpRequest, formData = new FormData;
+    xhr.responseType = 'json';
+    xhr.onload = () => {
+      if ( xhr.response.success ) {
+          callback( null, xhr.response );
+      } else {
+          callback( xhr.response )
+      }
+    }
+  
+    if ( method !== 'GET' ) {
+      formData.append( 'mail', email );
+      formData.append( 'password', password );
+      xhr.open( 'POST', newURL );
+      xhr.send( formData );   
+    } else {
+      xhr.open( 'GET', newURL );
+      xhr.send();
+    }
+  };
